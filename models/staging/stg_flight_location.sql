bafbb1cd5346bbe2c2fd538852596ac58f8c40cb
Amy Chen <amy@fishtownanalytics.com>
2023-09-13 12:41:44 +0000

    add dynamic table

--- models/staging/stg_flight_location.sql
+++ models/staging/stg_flight_location.sql
@@ -1,8 +1,12 @@
 {{
     config(
-        materialized='dynamic_table',
+        materialized = 'dynamic_table',
+        snowflake_warehouse = 'pc_dbt_wh',
+        target_lag = target_lag_environment(),
+        on_configuration_change = 'apply',
     )
 }}
+
 
 with source as (
 
@@ -13,13 +17,26 @@ with source as (
 
 renamed as (
 
-    select 
+    select distinct
 
         record_content:iss_position:latitude::string as latitude,
-        record_content:iss_position:longitude::string as longitude
+        record_content:iss_position:longitude::string as longitude,
+        record_content:message::string as status,
+        record_content:timestamp::timestamp as update_at
 
     from source
 
+),
+
+add_primary_key as(
+
+    select 
+        *,
+        {{ dbt_utils.generate_surrogate_key([
+            'update_at', 'latitude', 'longitude'])}} 
+            as primary_key
+
+    from renamed
 )
 
-select * from renamed
+select * from add_primary_key
